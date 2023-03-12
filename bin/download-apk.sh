c742193320a642ea57e3182b1a96bb6cebdbd7a1
Khaled Dallah <13232406+khaleddallah@users.noreply.github.com>
2023-03-12 16:19:02 +0100

    Update download-apk.sh

--- bin/download-apk.sh
+++ bin/download-apk.sh
@@ -2,4 +2,4 @@
 
 wget  --user-agent='Mozilla/5.0' \
       -O dexcom.stock.apk \
-      https://d.apkpure.com/b/APK/com.dexcom.g7?version=latest
+      "https://d-08.winudf.com/b/APK/Y29tLmRleGNvbS5nN18zOTA2XzIwMWZlMzM1?_fn=RGV4Y29tIEc3XzEuNC4wLjM5MDZfQXBrcHVyZS5hcGs&_p=Y29tLmRleGNvbS5nNw%3D%3D&is_hot=false&k=6ed6cbb6342671aaf33580d80814dd83640f3c0c"
