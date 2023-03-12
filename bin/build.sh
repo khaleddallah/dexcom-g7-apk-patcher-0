dfbb0c98e6e3faedf12830ee38e842df71818fd3
Khaled Dallah <13232406+khaleddallah@users.noreply.github.com>
2023-03-12 16:20:51 +0100

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -23,7 +23,7 @@ then
     echo "Please provide a valid .apk as first argument and try again!"
     echo "----------------------";
     echo "You can download the latest available .apk from here:"
-    echo "https://m.apkpure.com/de/dexcom-g7/com.dexcom.g7/download?from=details"
+    echo "https://d-08.winudf.com/b/APK/Y29tLmRleGNvbS5nN18zOTA2XzIwMWZlMzM1?_fn=RGV4Y29tIEc3XzEuNC4wLjM5MDZfQXBrcHVyZS5hcGs&_p=Y29tLmRleGNvbS5nNw%3D%3D&is_hot=false&k=6ed6cbb6342671aaf33580d80814dd83640f3c0c"
     echo "----------------------";
     echo "Execute script like bin/build.sh ./PATH_TO_DEXCOM_APK"
     exit;
